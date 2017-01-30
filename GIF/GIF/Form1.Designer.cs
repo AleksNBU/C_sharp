961659f5cc9b1549b870c69bdbcd1b6b1b0c5e39
Aleksandr Baranov <aleksaleksand1@gmail.com>
2017-01-30 16:01:08 +0200

    Create PNG from GIF

---
+++ GIF/GIF/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(120, 192);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 45);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Начать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(326, 58);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Определите файл GIF";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(77, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(243, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 19);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(65, 26);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Найти...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Location = new System.Drawing.Point(12, 86);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(326, 58);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Определите папку для кадров";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(77, 23);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(243, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Visible = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(6, 19);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(65, 26);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Найти...";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Enabled = false;
+            this.progressBar1.Location = new System.Drawing.Point(18, 153);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(314, 19);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 259);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Разбить gif\'ку покадрово";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        //void textBox1_MouseEnter(object sender, System.EventArgs e)
+        //{
+        //    throw new System.NotImplementedException();
+        //}
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ GIF/GIF/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        string FilesGif, Catalog;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.TextLength == 0)
+                MessageBox.Show("Найдите файл GIF!", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            else if (!File.Exists(FilesGif))
+                MessageBox.Show("Такого файла GIF не существует!", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else if (textBox2.TextLength == 0)
+                MessageBox.Show("Определите директорию для кадров!", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            else if (!Directory.Exists(Catalog))
+                MessageBox.Show("Такой директории не существует!", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+            {
+                using (Image gifImage = Image.FromFile(@FilesGif))
+                {
+                    FrameDimension dimension = new FrameDimension(gifImage.FrameDimensionsList[0]);
+                    progressBar1.Minimum = 0;
+                    progressBar1.Maximum = gifImage.GetFrameCount(dimension) - 1;
+                    progressBar1.Step = 1;
+                    for (int index = 0; index < gifImage.GetFrameCount(dimension) - 1; index++)
+                    {
+                        gifImage.SelectActiveFrame(dimension, index);
+                        using (Bitmap tmpBmp = new Bitmap(gifImage.Width, gifImage.Height, PixelFormat.Format32bppPArgb))
+                        using (Graphics g = Graphics.FromImage(tmpBmp))
+                        {
+                            g.DrawImage((Image)gifImage, 0, 0);
+                            tmpBmp.Save(string.Format(@Catalog + "\\{0:000}.png", index));
+                        }
+                        progressBar1.PerformStep();
+                    }
+                }
+                Text = new DirectoryInfo(@Catalog).GetFiles().Length.ToString();
+                MessageBox.Show("Количество файлов (кадров) в папке " + Text);
+                progressBar1.Value = 0;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = @"d:\Mydocuments\Изображения\";
+            openFileDialog1.Filter = "GIF files (*.gif)|*.gif";
+            openFileDialog1.Title = "Поиск GIF файла";
+            openFileDialog1.RestoreDirectory = true;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    FilesGif = openFileDialog1.FileName;
+                    textBox1.Text = FilesGif;
+                    textBox1.Visible = true;                   
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Файл GIF не может быть прочитан: " + ex.Message);
+                }
+             }
+           
+           }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Catalog = folderBrowserDialog1.SelectedPath;
+                textBox2.Text = Catalog + "\\";
+                textBox2.Visible = true;
+            }
+        }
+
+    }
+}
